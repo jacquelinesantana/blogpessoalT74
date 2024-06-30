27cc21cdcb7500be13411d8ea672043fd0eba628
jacquelinesantana <ti.jacque@gmail.com>
2024-06-30 14:32:30 -0300

    blog completo

--- src/main/java/com/generation/blogpessoal/controller/UsuarioController.java
+++ src/main/java/com/generation/blogpessoal/controller/UsuarioController.java
@@ -1,12 +1,15 @@
 package com.generation.blogpessoal.controller;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -47,5 +50,28 @@ public class UsuarioController {
 		return usuarioService.autenticarUsuario(usuarioLogin)
 				.map(resposta->ResponseEntity.status(HttpStatus.OK).body(resposta))
 				.orElse(ResponseEntity.status(HttpStatus.UNAUTHORIZED).build());
+	}
+	
+	@PutMapping("/atualizar")
+	public ResponseEntity<Usuario> putUsuario(@Valid @RequestBody Usuario usuario) {
+		
+		return usuarioService.atualizarUsuario(usuario)
+			.map(resposta -> ResponseEntity.status(HttpStatus.OK).body(resposta))
+			.orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+		
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<Usuario> getById(@PathVariable Long id) {
+		return usuarioRepository.findById(id)
+			.map(resposta -> ResponseEntity.ok(resposta))
+			.orElse(ResponseEntity.notFound().build());
+	}
+	
+	@GetMapping("/all")
+	public ResponseEntity <List<Usuario>> getAll(){
+		
+		return ResponseEntity.ok(usuarioRepository.findAll());
+		
 	}
 }
